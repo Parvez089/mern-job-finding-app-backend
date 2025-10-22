031428b1e48104c64a070660fe58ae9d4d01fc78
parvez <mhparvez089@gmail.com>
2025-10-22 23:13:13 +0600

    02: create jobs

---
+++ middleware/token.ts
@@ -0,0 +1,36 @@
+import type { Request, Response, NextFunction } from "express";
+import  type { JwtPayload } from "jsonwebtoken";
+import  jwt  from "jsonwebtoken";
+
+interface AuthenticatedRequest extends Request{
+  user?: string| JwtPayload;
+}
+
+export const verifyToken = (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+  try {
+    const token = req.header("Authorization")?.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({ message: "Access denied. No token provided." });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Invalid or expired token" });
+  }
+};
+
+
+export const checkRole = (roles: string[]) => {
+  return (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    const user = req.user as JwtPayload | undefined;
+    const userRole = user?.role;
+
+    if (!userRole || !roles.includes(userRole)) {
+      return res.status(403).json({ message: "Access denied. Insufficient permissions." });
+    }
+
+    next();
+  };
+};
