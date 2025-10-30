7f9fb80f7d6d46436786f22c0c505b4381fb1756
parvez <mhparvez089@gmail.com>
2025-10-30 18:19:39 +0600

    03_get_job fix

--- server.ts
+++ server.ts
@@ -7,9 +7,9 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-const port = 5000;
 
 
+const PORT = process.env.PORT || 5000;
 app.use(cors({
     origin: "http://localhost:3000",
 }));
@@ -23,6 +23,6 @@ app.use(express.json());
 app.use("/api/auth", userRoutes)
 app.use("/api/job", jobRoutes)
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
