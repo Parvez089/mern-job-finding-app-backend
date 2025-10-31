4d84b9979e9017e15a56fdebd98335e6cc6f8a05
parvez <mhparvez089@gmail.com>
2025-10-31 23:19:40 +0600

    04: Job route

--- server.ts
+++ server.ts
@@ -11,7 +11,9 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin:"http://localhost:3000",
+     methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
 }));
 dbConnect();
 
